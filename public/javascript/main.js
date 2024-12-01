b2d4da8ac9240530d9b95616e92cc0b3ceb109ca
alesbesdria <melissa@meirsman.fr>
2024-12-01 17:57:08 +0100

    projet2

---
+++ public/javascript/main.js
@@ -0,0 +1,7 @@
+const formsearchshow = document.querySelector(".searchbtn");
+const formsearch = document.querySelector(".mefform");
+formsearchshow.addEventListener('click', show);
+
+function show(){
+    navshow.classList.toggle("formcolorshow");
+}
