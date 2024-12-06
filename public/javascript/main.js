ee64d054ce9651a8204ff02443840b7ceebd59fa
alesbesdria <melissa@meirsman.fr>
2024-12-06 09:20:49 +0100

    projet2

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,7 +1,30 @@
-const formsearchshow = document.querySelector(".searchbtn");
-const formsearch = document.querySelector(".mefform");
-formsearchshow.addEventListener('click', show);
+const showvente = document.querySelector(".vente");
+const formsearchvente = document.querySelector(".searchvente");
 
-function show(){
-    navshow.classList.toggle("formcolorshow");
+const showlocation = document.querySelector(".location");
+const formsearchlocation = document.querySelector(".searchlocation");
+
+const showviager = document.querySelector(".viager");
+const formsearchviager = document.querySelector(".searchviager");
+
+showvente.addEventListener('click', showsearchvente);
+showlocation.addEventListener('click', showsearchlocation);
+showviager.addEventListener('click', showsearchviager);
+
+function showsearchvente(){
+    navshow.classList.show("formsearchvente");
+    navshow.classList.hide("formsearchlocation");
+    navshow.classList.hide("formsearchviager");
+}
+
+function showsearchlocation(){
+    navshow.classList.hide("formsearchvente");
+    navshow.classList.show("formsearchlocation");
+    navshow.classList.hide("formsearchviager");
+}
+
+function showsearchviager(){
+    navshow.classList.hide("formsearchvente");
+    navshow.classList.hide("formsearchlocation");
+    navshow.classList.show("formsearchviager");
 }
